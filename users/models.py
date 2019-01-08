a72102ac0e22ee0ddb6072c29686f92df8909d82
Alexander Korn <akorn@gustavus.edu>
2019-01-08 01:26:56 -0600

    added profile pics

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    steamid = models.CharField(default='0', max_length=20)
 
     def __str__(self):
         return f'{self.user.username} Profile'
